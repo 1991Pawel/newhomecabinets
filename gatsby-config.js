30e18a8cb398f36ace97c4faa6c54b6894b9a89e
Pawel L <virtuozoo@o2.pl>
2021-01-28 02:19:36 +0100

    init commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,35 +1,19 @@
 module.exports = {
+  siteMetadata: {
+    title: "My Gatsby Site",
+  },
   plugins: [
-    `gatsby-plugin-sass`,
-    `gatsby-plugin-react-helmet`,
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    `gatsby-plugin-offline`,
+    "gatsby-plugin-sass",
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-react-helmet",
+    "gatsby-transformer-sharp",
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: "gatsby-source-filesystem",
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images/`,
+        name: "images",
+        path: "./src/images/",
       },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `pages`,
-        path: `${__dirname}/src/pages/`,
-      },
-    },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Gatsby Simplefolio`,
-        short_name: `Simplefolio`,
-        start_url: `/`,
-        background_color: `#fff`,
-        theme_color: `#02aab0`,
-        display: `standalone`,
-        icon: 'src/images/favicon.png',
-      },
+      __key: "images",
     },
   ],
 };
