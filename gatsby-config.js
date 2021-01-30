04c1d7a5f0341b35d894a70a8cb8922e6fb6a49d
Pawel L <virtuozoo@o2.pl>
2021-01-30 14:44:18 +0100

    add manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,19 @@ module.exports = {
   },
   plugins: [
     "gatsby-plugin-sass",
-    "gatsby-plugin-sharp",
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
+      },
+    },
     "gatsby-plugin-react-helmet",
     "gatsby-transformer-sharp",
     {
